c973a922b8093ea96158e039fa23cf413caecdd1
mikeycgto <mikeycgto@gmail.com>
2011-08-12 19:30:10 -0400

    move scripts to included files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var quote, image, current;
+var setQuote = function(){
+	var index;
+	
+	do {
+		index = Math.floor(Math.random() * quotes.length);
+	} while (index == current);
+		
+	current = index;
+	quote.innerHTML = '"' + quotes[index] + '"';
+};
+	
+var setImage = function(){
+	var src = image.getAttribute('src');
+	var num = +src.match(/\d/)[0];
+		
+	num = num < 2 || num > 2 ? 2 : num + (Math.random() < 0.5 ? 1 : -1);
+	
+	image.setAttribute('src', src.replace(/\d/, num));
+};
+	
+window.onload = function(){
+	setTimeout(function(){ window.scrollTo(0, 1); }, 100);
+		
+	quote = document.getElementById('quote');
+	image = document.getElementById('image');
+		
+	setQuote();
+		
+	for (var i = 1; i <= 4; i+=2){
+		(new Image()).src = image.getAttribute('src').replace(/\d/, i);
+	}
+};
---
+++ js/quotes.js
@@ -0,0 +1,13 @@
+var quotes = [
+	"I've been quite open about this around the office: I don't want this parks department to build any parks, because I don't believe in government. I think that all government is a waste of taxpayer money. My dream is to have the park system privatized and run entirely for profit by corporations, like Chuck E. Cheese. They have an impeccable business model. I would rather work for Chuck E. Cheese.",
+	"My ex-wife Tammy cheated on me, then we divorced, then last week I ran into her sister Beth here; turns out she hates Tammy too, so we started dating. It's like a fairy tale.",
+	"On my deathbed, my final wish is to have my ex-wives rush to my side so I can use my dying breath to tell them both to go to hell one last time.",
+	"I got my first job when I was 9. Worked at a sheet metal factory. In two weeks, I was running the floor. Child labor laws are ruining this country.",
+	"I have cried twice in my life. Once when I was seven and I was hit by a school bus. And then again when I heard that Li'l Sebastian had passed.",
+	"I think if you would know one thing about me it would be that I prefer laying wreaths to lighting torches.",
+	"It's never too early to learn that the government is a greedy piglet that suckles on a taxpayer's teet until they have sore, chapped nipples. I'm gonna need a different metaphor to give this nine year old.",
+	"I won't publicly endorse a product unless I use it exclusively and I really believe in it. My only official recommendations are US Army issued mustache trimmers, Morton's Salt, and the C.R. Lawrence fine two inch style oscillating knife blade.",
+	"I don't like loud noises and people making a fuss. And I especially don't like people celebrating, because they know a piece of private information about me. Plus the whole thing is a scam. Birthdays were invented by Hallmark to sell cards.",
+	"The key to burning an ex-wife effigy is to dip it in paraffin wax and then toss the flaming bottle of isopropyl alcohol from a safe distance. Do not stand too close when you light an ex-wife effigy.",
+	
+];
