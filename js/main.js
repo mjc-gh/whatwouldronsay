d91097e5a7ce43f9c51a046ccde63c069b5edeb3
mikeycgto <mikeycgto@gmail.com>
2012-03-02 21:40:55 -0500

    better shuffle technique

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-var quote, image, current;
-var setQuote = function(){
-	var index;
+var quote, image, current = 0;
+
+// http://snippets.dzone.com/posts/show/849
+function shuffle(o){
+	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), 
+	  x = o[--i], o[i] = o[j], o[j] = x);
 	
-	do {
-		index = Math.floor(Math.random() * quotes.length);
-	} while (index == current);
-		
-	current = index;
-	quote.innerHTML = quotes[index].replace(/(\[[\w ]+\] )*([\w\W]+)/, function(str, m1, m2){
+	return o;
+};
+
+function setQuote(){
+	quote.innerHTML = quotes[current++].replace(/(\[[\w ]+\] )*([\w\W]+)/, function(str, m1, m2){
 		m2 = '"' + m2 + '"';
 		return m1 ? m1.slice(0,-1) + '<br>' + m2 : m2;
 	});
-};
 	
-var setImage = function(){
+	if (current == quotes.length)
+		current = 0;
+};
+
+function setImage(){
 	var src = image.getAttribute('src');
 	var num = +src.match(/\d/)[0];
 		
@@ -21,16 +26,17 @@ var setImage = function(){
 	
 	image.setAttribute('src', src.replace(/\d/, num));
 };
-	
+
 window.onload = function(){
 	setTimeout(function(){ window.scrollTo(0, 1); }, 100);
-		
+	
 	quote = document.getElementById('quote');
 	image = document.getElementById('image');
-		
+	
+	// shuffle the quotes
+	shuffle(quotes);
 	setQuote();
 		
-	for (var i = 1; i <= 4; i+=2){
+	for (var i = 1; i <= 4; i+=2)
 		(new Image()).src = image.getAttribute('src').replace(/\d/, i);
-	}
 };
